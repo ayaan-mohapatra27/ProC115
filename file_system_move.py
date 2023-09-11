87d0965761f2f1f7a062cc18daeba0e102545bca
ayaan-mohapatra27 <110197980+ayaan-mohapatra27@users.noreply.github.com>
2023-09-11 17:15:29 -0400

    Add files via upload

---
+++ file_system_move.py
@@ -0,0 +1,7 @@
+import os
+
+source="C:/Users/ayaan/OneDrive/Desktop/Coding/main.txt"
+destination="C:/Users/ayaan/OneDrive/Desktop/Coding/newfile.txt"
+
+os.rename(source,destination)
+print("Source Path Named for Destination Path Sucessfully")
